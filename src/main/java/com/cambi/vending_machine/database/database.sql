ec8fe93af4a6a243453f3da42c69dcbbbbed4bcb
danielowenscs <danielowenscs@gmail.com>
2023-06-21 17:32:19 -0400

    adding database

--- src/main/java/com/cambi/vending_machine/database/database.sql
+++ src/main/java/com/cambi/vending_machine/database/database.sql
@@ -1,47 +1,51 @@
 CREATE TABLE nutrient_group (
-	nutrient_group_id SERIAL PRIMARY KEY,
-	nutrient_group_name ENUM('macro', 'micro', 'ingredient') NOT NULL
+    nutrient_group_id SERIAL PRIMARY KEY,
+    nutrient_group_name varchar(20) NOT NULL
 );
 
 CREATE TABLE nutrient (
-	nutrient_id SERIAL PRIMARY KEY,
-	nutrient_name VARCHAR(50) NOT NULL,
-	unit ENUM('g', 'mg', 'μg', 'oz', NULL),
-	nutrient_group_id INT,
-	FOREIGN KEY (nutrient_group_id) REFERENCES nutrient_group (nutrient_group_id)
-);
-
-CREATE TABLE product (
-	product_id SERIAL PRIMARY KEY,
-	gtin_upc VARCHAR(14) NOT NULL
-);
-
-CREATE TABLE product_nutrient (
-	amount INT,
-	product_id INT,
-	nutrient_id INT,
-	FOREIGN KEY (product_id) REFERENCES product (product_id),
-	FOREIGN KEY (nutrient_id) REFERENCES nutrient (nutrient_id)
+    nutrient_id SERIAL PRIMARY KEY,
+    nutrient_name VARCHAR(50) NOT NULL,
+    unit varchar(10),
+    nutrient_group_id INT NOT NULL,
+    FOREIGN KEY (nutrient_group_id) REFERENCES nutrient_group (nutrient_group_id)
 );
 
 CREATE TABLE preference (
-	preference_id SERIAL PRIMARY KEY,
-	preference_name VARCHAR(50),
-	priority ENUM('0', '1', '2', '3') NOT NULL
+    preference_id SERIAL PRIMARY KEY,
+    preference_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE preference_priority (
+    preference_priority_id SERIAL PRIMARY KEY,
+    priority_level int NOT NULL,
+    preference_id INT NOT NULL,
+    FOREIGN KEY (preference_id) REFERENCES preference (preference_id)
+);
+
+CREATE TABLE preference_nutrient (
+    preference_nutrient_id SERIAL PRIMARY KEY,
+    nutrient_id INT NOT NULL,
+    preference_id INT NOT NULL,
+    FOREIGN KEY (nutrient_id) REFERENCES nutrient (nutrient_id),
+    FOREIGN KEY (preference_id) REFERENCES preference (preference_id)
+);
+
+CREATE TABLE preference_priority_nutrient (
+    preference_priority_nutrient_id SERIAL PRIMARY KEY,
+    preference_priority_id INT NOT NULL,
+    preference_nutrient_id INT NOT NULL,
+    FOREIGN KEY (preference_priority_id) REFERENCES preference_priority (preference_priority_id),
+    FOREIGN KEY (preference_nutrient_id) REFERENCES preference_nutrient (preference_nutrient_id)
 );
 
 CREATE TABLE nutrient_range (
-	minimum DECIMAL(5, 2),
-	maximum DECIMAL(5, 2),
-	slope DECIMAL(5, 2),
-	preference_id INT,
-	nutrient_id INT,
-	FOREIGN KEY (preference_id) REFERENCES preference (preference_id),
-	FOREIGN KEY (nutrient_id) REFERENCES nutrient (nutrient_id)
+    nutrient_range_id SERIAL PRIMARY KEY,
+    x_min DECIMAL(7, 2),
+    y_min DECIMAL(7, 2),
+    x_max DECIMAL(7, 2),
+    y_max DECIMAL(7, 2),
+    preference_priority_nutrient_id INT NOT NULL,
+    FOREIGN KEY (preference_priority_nutrient_id)
+        REFERENCES preference_priority_nutrient (preference_priority_nutrient_id)
 );
-
-
-
-
--- nutrient group table table would only include these 3 do I use ENUM or make it read only somehow
--- product table ups are at max 14 chars is this a good idea to restrict it to 14
