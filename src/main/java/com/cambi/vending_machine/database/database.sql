faa429e68f8ba78f9dcf23f181c180d91f3ec369
danielowenscs <danielowenscs@gmail.com>
2023-06-23 14:12:10 -0400

    adding database

--- src/main/java/com/cambi/vending_machine/database/database.sql
+++ src/main/java/com/cambi/vending_machine/database/database.sql
@@ -1,3 +1,9 @@
+DROP TABLE IF EXISTS nutrient_group CASCADE;
+DROP TABLE IF EXISTS nutrient CASCADE;
+DROP TABLE IF EXISTS preference CASCADE;
+DROP TABLE IF EXISTS preference_priority_nutrient CASCADE;
+DROP TABLE IF EXISTS nutrient_range CASCADE;
+
 CREATE TABLE nutrient_group (
     nutrient_group_id SERIAL PRIMARY KEY,
     nutrient_group_name varchar(20) NOT NULL
@@ -23,20 +29,13 @@ CREATE TABLE preference_priority (
     FOREIGN KEY (preference_id) REFERENCES preference (preference_id)
 );
 
-CREATE TABLE preference_nutrient (
-    preference_nutrient_id SERIAL PRIMARY KEY,
-    nutrient_id INT NOT NULL,
-    preference_id INT NOT NULL,
-    FOREIGN KEY (nutrient_id) REFERENCES nutrient (nutrient_id),
-    FOREIGN KEY (preference_id) REFERENCES preference (preference_id)
-);
+
 
 CREATE TABLE preference_priority_nutrient (
     preference_priority_nutrient_id SERIAL PRIMARY KEY,
     preference_priority_id INT NOT NULL,
     preference_nutrient_id INT NOT NULL,
-    FOREIGN KEY (preference_priority_id) REFERENCES preference_priority (preference_priority_id),
-    FOREIGN KEY (preference_nutrient_id) REFERENCES preference_nutrient (preference_nutrient_id)
+    FOREIGN KEY (preference_priority_id) REFERENCES preference_priority (preference_priority_id)
 );
 
 CREATE TABLE nutrient_range (
