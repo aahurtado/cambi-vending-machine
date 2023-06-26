c31b73879f85fe1ef9e0b767351f7f37d6b1be1b
danielowenscs <danielowenscs@gmail.com>
2023-06-26 19:39:19 -0400

    fixing formatting

--- src/main/java/com/cambi/vending_machine/controller/NutrientController.java
+++ src/main/java/com/cambi/vending_machine/controller/NutrientController.java
@@ -23,17 +23,14 @@ public class NutrientController {
     public void createNutrient (@RequestBody Nutrient nutrient) {
             nutrientDao.createNutrient(nutrient);
     }
-
     @GetMapping
     public Nutrient getNutrient(@RequestParam String nutrientName) throws Exception {
             return nutrientDao.getNutrient(nutrientName);
     }
-
     @PutMapping
     public void updateNutrient(@RequestBody Nutrient nutrient) throws Exception {
             nutrientDao.updateNutrient(nutrient);
     }
-
     @DeleteMapping
     public void deleteNutrient(@RequestParam String nutrientName) {
         nutrientDao.deleteNutrient(nutrientName);
